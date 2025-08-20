38ec68a2f0209581797e48c3b1614c9f51164e49
BarreraPro <dbarreradb1208@gmail.com>
2025-08-19 22:01:40 -0400

    Corregir test con emoji

--- index.test.js
+++ index.test.js
@@ -5,6 +5,6 @@ describe('GET /', () => {
   it('debe responder con Hola Mundo desde DevOps ', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
-    expect(res.text).toBe('Hola Mundo desde DevOps ');
+    expect(res.text).toBe('Hola Mundo desde DevOps 🚀');
   });
 });
